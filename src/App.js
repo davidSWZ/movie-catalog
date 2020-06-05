816c175585c7554ba390b1807e065731e0f07090
David Swiatkiewiez <davidswiatkiewiez@MacBook-Pro-de-David-Swiatkiewiez.local>
2020-06-05 22:36:02 +0200

    add title of the app

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="container"></div>;
+  return (
+    <div className="container">
+      <h1>Movie Kata.log</h1>
+    </div>
+  );
 }
 
 export default App;
