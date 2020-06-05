051f56d733db32f8f0ca09c66d689e8ed0fd1e48
David Swiatkiewiez <davidswiatkiewiez@MacBook-Pro-de-David-Swiatkiewiez.local>
2020-06-06 01:07:15 +0200

    add react-router-dom and create two routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 
 import SearchBox from "./components/SearchBox";
@@ -22,16 +23,29 @@ function App() {
   ]); //hook to handle the movies list
 
   return (
-    <div className="container">
-      <h1>Movie Kata.log</h1>
-      <SearchBox
-        fetchAPI={() => console.log("fetchAPI with: " + searchField)}
-        setSearchField={setSearchField}
-      />
-      <Scroll>
-        <CardList movies={movies} />
-      </Scroll>
-    </div>
+    // Router allow to switch from the two pages of the app
+    <Router>
+      <Switch>
+        {/*Route that display the root of the app, to search movies*/}
+        <Route exact path="/">
+          <div className="container">
+            <h1>Movie Kata.log</h1>
+            <SearchBox
+              fetchAPI={() => console.log("fetchAPI with: " + searchField)}
+              setSearchField={setSearchField}
+            />
+            <Scroll>
+              <CardList movies={movies} />
+            </Scroll>
+          </div>
+        </Route>
+
+        {/*Route that display the details of the selected movie*/}
+        <Route path="/:movieTitle/:movieId">
+          <p>Details Page</p>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
