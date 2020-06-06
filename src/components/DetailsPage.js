e83b92e07bd3705a411352e6efc800f11527c85a
David Swiatkiewiez <davidswiatkiewiez@MacBook-Pro-de-David-Swiatkiewiez.local>
2020-06-06 21:47:35 +0200

    check if movies are passed to the DetailsPage before rendering it

--- src/components/DetailsPage.js
+++ src/components/DetailsPage.js
@@ -7,6 +7,20 @@ import Poster from "./Poster";
 //Return a page with the details of a selected movie
 function DetailsPage({ movies }) {
   let { id } = useParams();
+  console.log(movies);
+
+  //Prevent someone to accidentally go to the DetailsPage and have an error
+  if (movies.length === 0)
+    return (
+      <div className="container">
+        <h1>Please come back to the landing page and search movies again</h1>
+        <Link to="/">
+          <button>Back to search</button>
+        </Link>
+      </div>
+    );
+
+  //Select the good movie in regard to the params
   const selectedMovie = movies.find(
     (movie) => movie.id.toString() === id.toString()
   );
