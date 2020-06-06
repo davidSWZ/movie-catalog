f3e90af0f47b7bba37c147f2f2805139c2498a0a
David Swiatkiewiez <davidswiatkiewiez@MacBook-Pro-de-David-Swiatkiewiez.local>
2020-06-06 23:18:01 +0200

    Get the name of the genres for each movie

--- src/components/api.js
+++ src/components/api.js
@@ -1,10 +1,28 @@
 //Call the TheMovieDB API, pass the API key and the searchfield parameter, and return the list of movies
 //matching the parameter
 
-const fetchAPI = async (searchField, request) => {
-  let fetchedData = await fetch(request); //Make the request
+const getMovies = async (searchField) => {
+  //Compose the URL for the request
+  const moviesRequest =
+    "https://api.themoviedb.org/3/search/movie?api_key=" +
+    process.env.REACT_APP_API_KEY +
+    "&query=" +
+    searchField;
+
+  let fetchedData = await fetch(moviesRequest); //Make the request
   let response = await fetchedData.json(); //Parse the result from the API
   return response.results; // return only the movies array from the response
 };
 
-export default fetchAPI;
+const getGenre = async () => {
+  //Compose the URL for the genre request
+  const genreRequest =
+    "https://api.themoviedb.org/3/genre/movie/list?api_key=" +
+    process.env.REACT_APP_API_KEY;
+
+  let fetchedData = await fetch(genreRequest); //Make the request
+  let response = await fetchedData.json(); //Parse the result from the API
+  return response; // return only the movies array from the response
+};
+
+export default { getMovies, getGenre };
--- src/components/handleMovieState.js
+++ src/components/handleMovieState.js
@@ -1,35 +1,34 @@
-import fetchApiMovies from "./api";
+import fetchApi from "./api";
+import GenreVSGenreId from "./GenreVSGenreId";
 
 //Take the response from the API and for each movie, keep only :
 //title, poster, ratings, genre and release date
-const handleMovieState = (handleMovies, searchField) => {
+
+function handleMovieState(handleMovies, searchField) {
   //create variable to compile only the needed informations from the film
   let newMovieList = [];
 
-  //Compose the URL for the movies request
-  const moviesRequest =
-    "https://api.themoviedb.org/3/search/movie?api_key=" +
-    process.env.REACT_APP_API_KEY +
-    "&query=" +
-    searchField;
-
-  //Call the API
-  fetchApiMovies(searchField, moviesRequest).then((moviesList) => {
-    if (!moviesList) return;
-    //then for each movie, only push the needed data to the newMovieList
-    moviesList.map((movie) => {
-      newMovieList.push({
-        id: movie.id,
-        poster: movie.poster_path,
-        title: movie.title,
-        rating: movie.vote_average,
-        genre: movie.genre_ids,
-        date: movie.release_date,
+  //Call the API for the genres
+  fetchApi.getGenre().then((genreList) => {
+    console.log(genreList);
+    //Call the API for the movies
+    fetchApi.getMovies(searchField).then((moviesList) => {
+      if (!moviesList) return;
+      //then for each movie, only push the needed data to the newMovieList
+      moviesList.map((movie) => {
+        newMovieList.push({
+          id: movie.id,
+          poster: movie.poster_path,
+          title: movie.title,
+          rating: movie.vote_average,
+          genre: GenreVSGenreId(genreList, movie.genre_ids),
+          date: movie.release_date,
+        });
+        return null;
       });
-      return null;
+      handleMovies(newMovieList);
     });
-    handleMovies(newMovieList);
   });
-};
+}
 
 export default handleMovieState;
